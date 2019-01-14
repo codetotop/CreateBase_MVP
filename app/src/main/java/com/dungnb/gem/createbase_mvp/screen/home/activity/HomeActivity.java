315aab8099744139d55e5156e1bbc6ae87c04ffd
DungNB <dungnb1@gemvietnam.com>
2019-01-14 17:51:59 +0700

    Add BottomNavigationBar and Home Fragment .

--- app/src/main/java/com/dungnb/gem/createbase_mvp/screen/home/activity/HomeActivity.java
+++ app/src/main/java/com/dungnb/gem/createbase_mvp/screen/home/activity/HomeActivity.java
@@ -1,19 +1,28 @@
 package com.dungnb.gem.createbase_mvp.screen.home.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 import com.dungnb.gem.createbase_mvp.R;
 import com.dungnb.gem.createbase_mvp.base.activity.mvp.BaseActivityView;
 import com.dungnb.gem.createbase_mvp.screen.home.fragment.HomeFragment;
 import com.dungnb.gem.createbase_mvp.screen.home.fragment.HomeFragmentPresenter;
 
-public class HomeActivity extends BaseActivityView<HomeActivityContract.HomeActivityPresenter> implements HomeActivityContract.HomeActivityView {
+import butterknife.BindView;
+
+public class HomeActivity extends BaseActivityView<HomeActivityContract.HomeActivityPresenter> implements HomeActivityContract.HomeActivityView, BottomNavigationView.OnNavigationItemSelectedListener {
+
+  @BindView(R.id.bottomBarView)
+  BottomNavigationView mBottomNavigationView;
 
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    addOrShowChildrenFragment(new HomeFragmentPresenter().getFragment(), R.id.container, false, HomeFragment.class.getSimpleName());
+    mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+
   }
 
   @Override
@@ -27,4 +36,19 @@ public class HomeActivity extends BaseActivityView<HomeActivityContract.HomeActi
   }
 
 
+  @Override
+  public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    switch (menuItem.getItemId()) {
+      case R.id.menuHome:
+        addOrShowChildrenFragment(new HomeFragmentPresenter().getFragment(), R.id.container, false, HomeFragment.class.getSimpleName());
+        break;
+      case R.id.menuHistory:
+        break;
+      case R.id.menuMore:
+        break;
+      default:
+        break;
+    }
+    return false;
+  }
 }
